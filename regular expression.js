267b132d99ae2878754e7431f95f15e08dc95ad4
Fahim Muntasir <niloyf3@gmail.com>
2022-10-05 00:41:25 +0600

    match function

--- regular expression.js
+++ regular expression.js
@@ -2,7 +2,7 @@
 // these are used to match part of strings. 
 
 let bigString = "a quick brown fox jumps over the lazy dog"
-let testRegex = /quick/g
+let testRegex = /quick/g //global idk what
 console.log(testRegex.test(bigString))
 
 
@@ -14,3 +14,8 @@ console.log(multiRegex.test(bigString));
 // regex can also be set so that it ignores the lettercase
 let ignoreRegex = /DOG|CAT|GOAT/i
 console.log(ignoreRegex.test(bigString));
+
+// Match function matches the exact pattern that we tell it to find
+// it returns with an index position object
+let matchRegex = /jumps/i //ignore case can be applied here as well
+console.log(bigString.match(matchRegex));
