255e2d063aa1d8b163e425b3006bdefba8da422b
Fahim Muntasir <niloyf3@gmail.com>
2022-10-05 00:34:23 +0600

    various test methods

--- regular expression.js
+++ regular expression.js
@@ -1,7 +1,16 @@
 // regular expressions are often called `regex`
-// these are used to match part of strings.
+// these are used to match part of strings. 
 
 let bigString = "a quick brown fox jumps over the lazy dog"
-let testRegex = /over/g
-let res = testRegex.test(bigString)
-console.log(res)
+let testRegex = /quick/g
+console.log(testRegex.test(bigString))
+
+
+// regex can take multiple values to match. these are separated by or (|) operation
+let multiRegex = /cat|dog|rabbit/
+console.log(multiRegex.test(bigString));
+
+
+// regex can also be set so that it ignores the lettercase
+let ignoreRegex = /DOG|CAT|GOAT/i
+console.log(ignoreRegex.test(bigString));
